906e1abb993842dad8cd9c03c371a64413f67f11
vishnugowtham97 <vishnugowtham97@gmail.com>
2023-04-21 18:16:48 +0530

    Add commit

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+    nav = document.getElementById(navId);
 
-  if(toggle && nav){
-        toggle.addEventListener('click', () =>{
-              nav.classList.toggle('show')
-        });
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
   }
-}
+};
 
-showMenu('nav_toggle','nav_menu')
+showMenu("nav_toggle", "nav_menu");
 
 // ACTIVE & REMOVE ACTIVE
-const navLink = document.querySelectorAll('.nav_link')
-navLink.forEach(n => n.classList.remove('active'))
+const navLink = document.querySelectorAll(".nav_link");
+navLink.forEach((n) => n.classList.remove("active"));
 
-function linkAction(){
-  navLink.forEach(n => n.classList.remove('active'))
-  this.classList.add('active')
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
 
-  const navMenu = document.getElementById('nav_menu')
-  navMenu.classList.remove('show')
+  const navMenu = document.getElementById("nav_menu");
+  navMenu.classList.remove("show");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+function type() {
+  let name1 = " VISHNU";
+  let namearr = name1.split("");
+
+  function looping() {
+    if (namearr.length > 0) {
+      let n = namearr.shift();
+      document.querySelector("#heading--main").innerHTML += n;
+    } else {
+      //deleting();
+      return false;
+    }
+    setTimeout(looping, 900);
+  }
+
+  looping();
+}
+
+type();
+
+const words = [
+  "Full Stack Developer",
+  "Front End Developer",
+  "Back End Developer",
+  "Web Developer",
+  "Programmer",
+];
+let currentWordIndex = 0;
+const paragraphElement = document.getElementById("course");
+
+function updateParagraph() {
+  paragraphElement.innerText = words[currentWordIndex];
+  currentWordIndex = (currentWordIndex + 1) % words.length;
+}
+
+setInterval(updateParagraph, 1000);
